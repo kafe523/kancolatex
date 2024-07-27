d10d156b93cbe7fb3ec6d77df4530a4411fb0dcb
kafe523 <kafe.chan1@gmail.com>
2024-07-28 00:41:59 +0800

    fix wrong deckbuilder airbase wrong type

--- src/kancolatex/types/deck_builder/serialization/airbase.py
+++ src/kancolatex/types/deck_builder/serialization/airbase.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from pydantic import Field
 from typing_extensions import Optional
 
-from ...const import AIR_STATE
+from ...const import AirBaseActionKind
 from .airbase_equipment import DeckBuilderAirBaseEquipmentList
 
 
 class DeckBuilderAirBase(BaseModel):
     name: str = Field(alias="name")
     equipment: DeckBuilderAirBaseEquipmentList = Field(alias="items")
-    mode: AIR_STATE = Field(alias="mode")
+    mode: AirBaseActionKind = Field(alias="mode")
     distance: int = Field("distance")
     strikePoint: Optional[list[int]] = Field(alias="sp", default=None)
